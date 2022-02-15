821ca0051adc8983faf58174e71d8bac56e6d0eb
jaime <raciondecroquetas21@gmail.com>
2022-02-15 20:55:05 +0100

    rick

--- home_normal.php
+++ home_normal.php
@@ -15,7 +15,7 @@
             <img src="https://m.media-amazon.com/images/I/61TcIu-z18L.jpg" alt="cowboy bebop">
             <h2>Fatboy Slim</h2>
             <audio controls="controls">
-                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+                <source src="img\yt1s.com - Rick Astley  Never Gonna Give You Up video oficial  Español.mp3" type="audio/mpeg">
             </audio>
 
         </article>
@@ -25,7 +25,7 @@
             <img src="https://i1.sndcdn.com/artworks-000500823264-7n2dz6-t500x500.jpg" alt="samurai">
             <h2>Queen</h2>
             <audio controls="controls">
-                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+                <source src="img\yt1s.com - Rick Astley  Never Gonna Give You Up video oficial  Español.mp3" type="audio/mpeg">
             </audio>
         </article>
 
@@ -35,7 +35,7 @@
             <img src="https://i.discogs.com/Wt-KEarJuu0egKaDL2TtF3dmhWf5sOMNG997GabrapE/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWltYWdlcy9SLTI1/MjAzMDAtMTYxOTI1/NTQ5Ny00OTQzLmpw/ZWc.jpeg" alt="cowboy bebop">
             <h2>AC DC</h2>
             <audio controls="controls">
-                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+                <source src="img\yt1s.com - Rick Astley  Never Gonna Give You Up video oficial  Español.mp3" type="audio/mpeg">
             </audio>
 
         </article>
@@ -45,7 +45,7 @@
             <img src="https://i1.sndcdn.com/artworks-000114356453-0cgzxm-t500x500.jpg" alt="samurai">
             <h2>Bon Jovi</h2>
             <audio controls="controls">
-                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+                <source src="img\yt1s.com - Rick Astley  Never Gonna Give You Up video oficial  Español.mp3" type="audio/mpeg">
             </audio>
         </article>
 
@@ -53,7 +53,7 @@
             <img src="https://i1.sndcdn.com/artworks-pQVTElgSdRykAivX-IliaIQ-t500x500.jpg" alt="cowboy bebop">
             <h2>Metallica</h2>
             <audio controls="controls">
-                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+                <source src="img\yt1s.com - Rick Astley  Never Gonna Give You Up video oficial  Español.mp3" type="audio/mpeg">
             </audio>
         </article>
 
@@ -62,21 +62,21 @@
             <img src="https://upload.wikimedia.org/wikipedia/en/7/79/Yesterday_EP.jpg" alt="samurai">
             <h2>The Beatles</h2>
             <audio controls="controls">
-                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+                <source src="img\yt1s.com - Rick Astley  Never Gonna Give You Up video oficial  Español.mp3" type="audio/mpeg">
             </audio>
         </article>
         <article>
             <img src="https://www.villamercedes.info/wp-content/uploads/2021/09/nevermind.jpg" alt="samurai">
             <h2>nirvana</h2>
             <audio controls="controls">
-                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+                <source src="img\yt1s.com - Rick Astley  Never Gonna Give You Up video oficial  Español.mp3" type="audio/mpeg">
             </audio>
         </article>
         <article>
             <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQhfSccxtXf4-ku8706Stn6pRCw2NjjR6p49CktBcUhyel12INl2-XbqBzMZIcxKkK0m477xMWOkK5fqBeSTmpyokq82Fr8D5qYLqmAykE&usqp=CAE" alt="samurai">
             <h2>Pink floyd</h2>
             <audio controls="controls">
-                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+                <source src="img\yt1s.com - Rick Astley  Never Gonna Give You Up video oficial  Español.mp3" type="audio/mpeg">
             </audio>
         </article>
    
