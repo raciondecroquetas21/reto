2201fea778f53c87e3ff88b0481de7e07afdf7ac
jaime <raciondecroquetas21@gmail.com>
2022-02-15 16:18:35 +0100

    controles home

--- home_normal.php
+++ home_normal.php
@@ -13,7 +13,9 @@
         <article>
             <img src="https://m.media-amazon.com/images/I/61TcIu-z18L.jpg" alt="cowboy bebop">
             <h2>Fatboy Slim</h2>
-            <audio id="sonido" src="https://soundcloud.com/src-music/sacrificio-xundah-flex-feat-triplo-feat-costa-hip-hop" controls>
+            <audio controls="controls">
+                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+            </audio>
 
         </article>
 
@@ -30,7 +32,9 @@
         <article>
             <img src="https://i1.sndcdn.com/artworks-qgtY5AzcWhrOnCZc-BA2mIw-t500x500.jpg" alt="cowboy bebop">
             <h2>hellsing</h2>
-            <button>ir a la serie</button>
+            <audio controls="controls">
+                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+            </audio>
 
         </article>
 
@@ -38,53 +42,61 @@
         <article>
             <img src="https://images.chesscomfiles.com/uploads/v1/blog/440456.4dbd394c.5000x5000o.dea2389f0442.jpeg" alt="samurai">
             <h2>bersek</h2>
-            <button>ir a la serie</button>
-
+            <audio controls="controls">
+                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+            </audio>
         </article>
 
         <article>
             <img src="https://i1.sndcdn.com/artworks-pQVTElgSdRykAivX-IliaIQ-t500x500.jpg" alt="cowboy bebop">
             <h2>afro samurai</h2>
-            <button>ir a la serie</button>
-
+            <audio controls="controls">
+                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+            </audio>
         </article>
 
 
         <article>
             <img src="https://upload.wikimedia.org/wikipedia/en/7/79/Yesterday_EP.jpg" alt="samurai">
             <h2>bleach</h2>
-            <button>ir a la serie</button>
-
+            <audio controls="controls">
+                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+            </audio>
         </article>
         <article>
             <img src="https://m.media-amazon.com/images/M/MV5BMDZiYmIxNGItMjYwZS00ZjQ2LTg3ZmUtNGYzOTczN2M0NjQ2XkEyXkFqcGdeQXVyNDQ5MDYzMTk@._V1_.jpg" alt="samurai">
             <h2>bleach</h2>
-            <button>ir a la serie</button>
-
+            <audio controls="controls">
+                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+            </audio>
         </article>
         <article>
             <img src="https://i.pinimg.com/originals/ff/c6/b3/ffc6b31c2f7893ac675d6f41247837c8.png" alt="samurai">
             <h2>bleach</h2>
-            <button>ir a la serie</button>
-
+            <audio controls="controls">
+                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+            </audio>
         </article>
         <article>
             <img src="https://fringster.com/content/images/12678.jpg" alt="samurai">
             <h2>bleach</h2>
-            <button>ir a la serie</button>
-
+            <audio controls="controls">
+                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+            </audio>
         </article>
         <article>
             <img src="https://images-na.ssl-images-amazon.com/images/I/41M16VPNAQL._AC_.jpg" alt="samurai">
             <h2>bleach</h2>
-            <button>ir a la serie</button>
-
+            <audio controls="controls">
+                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+            </audio>
         </article>
         <article>
             <img src="https://m.media-amazon.com/images/M/MV5BYzlhZDdjNjctNDRiZS00NjI2LWJmOTQtODc4YmY3OWY2NWNjXkEyXkFqcGdeQXVyNDQ5MDYzMTk@._V1_.jpg" alt="samurai">
             <h2>bleach</h2>
-            <button>ir a la serie</button>
-
+            <audio controls="controls">
+                <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+            </audio>
         </article>
 
 
