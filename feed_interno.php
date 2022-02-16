fb71905ea2cb95b977b33812cd0dea3b03791853
raciondecroquetas21 <96258717+raciondecroquetas21@users.noreply.github.com>
2022-02-16 15:06:45 +0000

    feed interno

--- feed_interno.php
+++ feed_interno.php
@@ -1,12 +1,40 @@
 <?php
             include_once 'inc/db.php';
-            include_once 'inc/nav.html';
+
+            //https://codigolite.com/como-crear-ejecutar-y-eliminar-un-procedimiento-almacenado-en-oracle/
+      
+      
+            ////    meter cosas oracle en php https://programmerclick.com/article/5191939387/   
+            BEGIN
+                noticias_orden ();
+            COMMIT;
+         END;
+////////////////////////////////////////////////////
+            PROCEDURE NOTICIAS_ORDEN
+            AS
+            CURSOR ORDEN_N IS
+            SELECT titulo, contenido FROM noticias_web order by fecha_noticias desc;
+            v_titulo varchar2(60);
+            v_contenido varchar2(1000);
+            BEGIN
+            OPEN ORDEN_N;
+            FETCH ORDEN_N INTO v_titulo, v_contenido;
+            WHILE ORDEN_N%FOUND LOOP
+            DBMS_OUTPUT.PUT_LINE(V_TITULO||'*'||V_CONTENIDO);
+            FETCH ORDEN_N INTO v_titulo, v_contenido;
+            END LOOP;
+            CLOSE ORDEN_N;
+            END NOTICIAS_ORDEN;
+
+            for ($i=0; $i < ; $i++) { 
+                # code...
+            }
 
             ?>
 
 
 <?php 
-    $conn = oci_connect('A.Asburgo','Almi123') or die;
+    $conn = oci_connect('SCOTT','TIGER') or die;
     
     
     $sql = 'BEGIN sayHello(:name, :message); END;';
@@ -26,4 +54,16 @@
     
     // $message is now populated with the output value
     print "$message\n";
+?>
+
+
+<?php 
+   
+    include_once 'inc/db.php';
+
+    $sql = 'BEGIN noticias_orden(); END;';
+    
+    $stmt = oci_parse($conn,$sql);
+    
+    oci_execute($stmt); 
 ?>
