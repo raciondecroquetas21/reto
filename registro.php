1967bb84111d1b76ce50e0110d9b445e4a09d89b
jaime <raciondecroquetas21@gmail.com>
2022-02-16 15:27:14 +0100

    correccion ruta css

--- registro.php
+++ registro.php
@@ -15,7 +15,7 @@
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Document</title>
-                <link rel="stylesheet" href="registro.css">
+                <link rel="stylesheet" href="css/registro.css">
             </head>
             <body>
                     <form action="" method="post">
