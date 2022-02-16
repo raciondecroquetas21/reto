06c7316a23b8b701686af6ef33bf40a3f08d8081
jaime <raciondecroquetas21@gmail.com>
2022-02-16 16:53:01 +0100

    css fondos y estilo del registro

--- feed_web.php
+++ feed_web.php
@@ -1,5 +1,5 @@
 <?php
-            include_once 'inc/db.php';
+            
             include_once 'inc/nav.html';
 
 
@@ -11,6 +11,51 @@
 
 
 <?php
+////////////////////////////////////////////////
+//********PARSE YOUTUBE XML FEED WITH PHP*****//
+//******************By Nancy O.***************//
+//*******Alt-Web Design & Publishing******//
+//**************https://alt-web.com************//
+//*********SAVE THIS FILE AS MY_FEED.PHP******//
+////////////////////////////////////////////////
+$html = "";
+//URL of your XML feed by user, playlist or channel ID
+$feed  = "https://www.youtube.com/feeds/videos.xml?channel_id=UCWFRESFCQjtDv0egKR3j40Q";
+//Load feed xml file
+$xml = simplexml_load_file($feed);
+//display 6 feed entries, use more or less as desired
+for($i = 0; $i < 6; $i++) {
+   //define our feed nodes
+   $published = $xml->entry[$i]->published;
+   //optional, shorten the date
+   $shortDate = date("m/d/Y", strtotime($published));
+   $title = $xml->entry[$i]->title;
+   $id = $xml->entry[$i]->id;
+   //strip unwanted characters from ID
+   $id = str_replace ("yt:video:", "", $id);
+   $author = $xml->entry[$i]->author->name;
+   $uri = $xml->entry[$i]->author->uri;
+   //put nodes into html tags & embedded youTube player.
+   $html .= "<div class='col-sm-6 col-md-4'>
+<h4><a href='$uri'>$title</a></h4>
+<iframe src='http://www.youtube.com/embed/$id' allowfullscreen>
+</iframe><br>
+<small>Published: $shortDate &nbsp; By: $author></small>
+</div><hr>";
+   }
+//output everything to html
+echo $html;
+?>
+
+
+
+
+
+
+
+
+<!-- 
+
 $rss = simplexml_load_file('https://www.billboard.com/feed/');
 
 echo '<h4>'. $rss->channel->title . '</h4>';
@@ -20,7 +65,7 @@ foreach ($rss->channel->item as $item) {
    echo "<p>" . $item->title . "</p>";
    echo "<p>" . $item->description . "</p>";
 } 
-?>
 
 
+ -->
 
