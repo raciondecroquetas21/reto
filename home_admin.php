0995941c3652d8e1fa98a96b200fb7f139d775dc
jaime <raciondecroquetas21@gmail.com>
2022-02-16 15:15:33 +0100

    home_admin meter mp3

--- home_admin.php
+++ home_admin.php
@@ -15,21 +15,21 @@ include_once 'home_normal.php';
     <img src="https://img.discogs.com/zuBxCMR0x_filp64r3TmapSJqrg=/fit-in/300x300/filters:strip_icc():format(webp):mode_rgb():quality(40)/discogs-images/R-10130983-1540837435-8429.jpeg.jpg" alt="samurai">
     <h2>scorpions</h2>
     <audio controls="controls">
-        <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+        <source src="img\yt1s.com - Rick Astley  Never Gonna Give You Up video oficial  Español.mp3 type="audio/mpeg">
     </audio>
     </article>
     <article>
     <img src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSJMxXYd4ZKHamVAd7XXKGuWRxxs5MlOmUKZ64fRbdkQQOOdxsSTtC38kbrv_vyDvmmuF-4-kG3Wpn7El_uALwXPsnwq12Vo4xaUF1-7p_TSPFoz4gUxqbP&usqp=CAY" alt="samurai">
     <h2>Daft punk</h2>
     <audio controls="controls">
-        <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+        <source src="img\yt1s.com - Rick Astley  Never Gonna Give You Up video oficial  Español.mp3 type="audio/mpeg">
     </audio>
     </article>
     <article>
     <img src="https://i1.sndcdn.com/artworks-ECD4kPnRBh4NzV2S-bB2JZA-t500x500.jpg" alt="samurai">
     <h2>Jimmy Hendrix</h2>
     <audio controls="controls">
-        <source src="https://freesound.org/people/DaveJf/sounds/619891/" type="audio/mpeg">
+        <source src="img\yt1s.com - Rick Astley  Never Gonna Give You Up video oficial  Español.mp3 type="audio/mpeg">
     </audio>
     </article>
  </main>
