4f322adf1ffb9f7669e7a686db1ac648dbe7a549
raciondecroquetas21 <96258717+raciondecroquetas21@users.noreply.github.com>
2022-02-17 16:09:32 +0000

    aa

--- inc/registrar.php
+++ inc/registrar.php
@@ -3,15 +3,15 @@
 
 include_once 'db.php';
 
-/*function insertar_usuario($conn,$dni, $nombre, $apellido, $direccion, $localidad, $nombre_usuario, $password)
-    */
-        /*$sql = "INSERT INTO clientes VALUES ('".$dni."','".$nombre."','".$apellido."','".$direccion."','".$localidad."','".$nombre_usuario."','".$password."')";
+function insertar_usuario($conn,$dni, $nombre, $apellido, $direccion, $localidad, $nombre_usuario, $password)
+    
+    $sql = "INSERT INTO clientes VALUES ('".$dni."','".$nombre."','".$apellido."','".$direccion."','".$localidad."','".$nombre_usuario."','".$password."')";
          $stmt = oci_parse($conexion, $sql);     
          $ok   = oci_execute( $stmt );          
          oci_free_statement($stmt);             
-        return $ok;*/
-
-        $stid = oci_parse($conn,"INSERT INTO clientes VALUES( :dni , :nombre, :apellido, :direccion, :localidad, :nom_usu, :con_usu)");
+        return $ok;
+    
+         /* $stid = oci_parse($conn,"INSERT INTO clientes VALUES( :dni , :nombre, :apellido, :direccion, :localidad, :nom_usu, :con_usu)");
 
         $dni=$_POST['dni'];
         $nombre=$_POST['nombre'];
@@ -29,6 +29,6 @@ include_once 'db.php';
         oci_bind_by_name($stid, ":localidad", $localidad);
         oci_bind_by_name($stid, ":nom_usu", $nombre_usuario);
         oci_bind_by_name($stid, ":con_usu", $password);
-        oci_execute($stid);
+        oci_execute($stid);*/
    
 ?>
