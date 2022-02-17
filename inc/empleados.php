09ac7d1f52b26907875aefca2a0848f9b77883ae
raciondecroquetas21 <96258717+raciondecroquetas21@users.noreply.github.com>
2022-02-17 16:29:12 +0000

    qq

---
+++ inc/empleados.php
@@ -0,0 +1,30 @@
+<?php
+
+    session_start();
+    include_once 'db.php';
+    if (!$conn) { $m = oci_error();
+    echo $m['message'], '\n';
+    exit;}
+    $query = 'SELECT *  FROM empleados WHERE nombre_usuario_empleado = :usu and contrasena_empleado = :pwd' ;
+    $stid = oci_parse($conn, $query);
+    if (isset($_POST['usu']) || isset($_POST['pwd'])){          
+    $user=$_POST['usu'];
+    $pass=$_POST['pwd'];
+    }
+    oci_bind_by_name($stid, ':usu', $user);
+    oci_bind_by_name($stid, ':pwd', $pass);
+    oci_execute($stid);
+    $row = oci_fetch_array($stid, OCI_ASSOC);
+    if ($row) {
+    $_SESSION['usu']=$_POST['usu'];
+    echo'log in successful';
+    }
+    else {
+    header('Location: ../index.php');
+    exit;}
+
+    oci_free_statement($stid);
+    oci_close($conn);
+    header('Location: ../home_admin.php');
+
+?>
