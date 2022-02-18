7f0d2cab67e3e66b28ddecf46c39aba1f4e3f1e8
raciondecroquetas21 <96258717+raciondecroquetas21@users.noreply.github.com>
2022-02-18 15:34:23 +0000

    qw

--- feed_interno.php
+++ feed_interno.php
@@ -95,7 +95,7 @@ echo $rss->asXML(); //output XML */
 
   include_once 'inc/db.php';
 
-  $stmt=oci_parse ($conn, "BEGIN noticias_orden(); END;");
+  $stmt=oci_parse ($conn, "BEGIN noticias_orden; END;");
 
        if ($result = oci_execute($stmt)) {
 
