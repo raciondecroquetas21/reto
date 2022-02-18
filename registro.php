e1e1b97dd26df7d37ef69e65eaabbbbc795242ce
jaime <raciondecroquetas21@gmail.com>
2022-02-18 16:39:32 +0100

    dd

--- registro.php
+++ registro.php
@@ -10,7 +10,7 @@
                 <meta charset="UTF-8">
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
+                <title>Registro</title>
                 <link rel="stylesheet" href="css/registro.css">
             </head>
             <body>
@@ -33,4 +33,4 @@
                         <input type="submit" name="submit">
                     </form>
             </body>
-            </html>
+</html>
