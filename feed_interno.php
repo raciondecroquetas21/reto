829d16aada3dabb32fcc48e3a7c2a6096d448edb
raciondecroquetas21 <96258717+raciondecroquetas21@users.noreply.github.com>
2022-02-18 14:26:17 +0000

    AA

--- feed_interno.php
+++ feed_interno.php
@@ -1,12 +1,12 @@
 <?php 
    
-    include_once 'inc/db.php';
+   /* include_once 'inc/db.php';
 
     $sql = 'BEGIN noticias_orden(); END;';
     
     $stmt = oci_parse($conn,$sql);
     
-    oci_execute($stmt); 
+    oci_execute($stmt); */
 
 
 
