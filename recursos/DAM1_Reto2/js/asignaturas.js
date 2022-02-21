a45ed1109ce6e163dcf4fe109230052323a65525
jaime <raciondecroquetas21@gmail.com>
2022-02-21 15:55:59 +0100

    borrar

--- recursos/DAM1_Reto2/js/asignaturas.js
+++
@@ -1,5 +0,0 @@
-function asignaturas() {
-  if (window.location.href.indexOf('?seccion=') > 0 ) {
-    document.getElementById("cajaasignaturas").style.display = "block";
-  }
-}
--- recursos/WebReto/inc/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-// unset($_SESSION['user']);
-header("Location: ../index.php");
